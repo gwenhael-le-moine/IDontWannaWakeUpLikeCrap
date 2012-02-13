1b9903440c06cf464b4e5ac38add66b53164886a
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2012-02-13 23:39:45 +0100

    Born again (and again (and again (and again)))

--- src/com/gwenhael/idontwannawakeuplikecrap/IDontWannaWakeUpLikeCrapActivity.java
+++ src/com/gwenhael/idontwannawakeuplikecrap/IDontWannaWakeUpLikeCrapActivity.java
@@ -2,8 +2,13 @@ package com.gwenhael.idontwannawakeuplikecrap;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class IDontWannaWakeUpLikeCrapActivity extends Activity
+public class IDontWannaWakeUpLikeCrapActivity
+    extends Activity
+    implements OnClickListener
 {
     /** Called when the activity is first created. */
     @Override
@@ -11,5 +16,10 @@ public class IDontWannaWakeUpLikeCrapActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void onClick( View view )
+    {
+        Toast.makeText(this, "Coucou n°2", Toast.LENGTH_SHORT).show();
     }
 }
