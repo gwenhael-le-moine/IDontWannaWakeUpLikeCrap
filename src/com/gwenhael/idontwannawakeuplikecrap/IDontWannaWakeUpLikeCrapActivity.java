36fd0e828708c9ec2ae4ce0648120a59a1c78aaf
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2012-02-13 23:57:24 +0100

    Success, more of it, Yay [o/

--- src/com/gwenhael/idontwannawakeuplikecrap/IDontWannaWakeUpLikeCrapActivity.java
+++ src/com/gwenhael/idontwannawakeuplikecrap/IDontWannaWakeUpLikeCrapActivity.java
@@ -5,21 +5,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Button;
 
 public class IDontWannaWakeUpLikeCrapActivity
     extends Activity
     implements OnClickListener
 {
+    protected EditText falling_asleep_time;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main); // 
+        
+        falling_asleep_time = (EditText) findViewById( R.id.falling_asleep_time );
     }
 
     public void onClick( View view )
     {
-        Toast.makeText(this, "Coucou n°2", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, falling_asleep_time.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
