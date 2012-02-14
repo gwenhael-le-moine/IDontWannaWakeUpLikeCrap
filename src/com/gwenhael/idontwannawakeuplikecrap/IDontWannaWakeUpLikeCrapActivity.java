46dffac86da1f8b7d4a43346aeba3bb4ed1e72a2
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2012-02-14 23:08:24 +0100

    laughingly Minimal, sub-barely Viable Product

--- src/com/gwenhael/idontwannawakeuplikecrap/IDontWannaWakeUpLikeCrapActivity.java
+++ src/com/gwenhael/idontwannawakeuplikecrap/IDontWannaWakeUpLikeCrapActivity.java
@@ -4,11 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Toast;
+// import android.widget.Toast;
 import android.widget.TextView;
 import android.widget.EditText;
-import android.widget.TimePicker;
-import android.widget.Button;
+// import android.widget.TimePicker;
+// import android.widget.Button;
+import java.util.Calendar;
 
 public class IDontWannaWakeUpLikeCrapActivity
     extends Activity
@@ -31,8 +32,28 @@ public class IDontWannaWakeUpLikeCrapActivity
     @Override
     public void onClick( View view )
     {
-        Toast.makeText(this, falling_asleep_time.getText().toString(), Toast.LENGTH_SHORT).show();
+        // Toast.makeText(this, falling_asleep_time.getText().toString(), Toast.LENGTH_SHORT).show();
+        int time_to_fall_asleep = (new Integer( falling_asleep_time.getText().toString() )).intValue(  );
 
-        result_text.setText( falling_asleep_time.getText().toString() );
+        Calendar now = Calendar.getInstance();
+        Calendar asleep = (Calendar)now.clone(  ); // what's with the casting crap?
+        asleep.add( Calendar.MINUTE, time_to_fall_asleep );
+        Calendar wake_up = (Calendar)asleep.clone(  );
+        
+        String r = new String(  );
+        for ( int i = 0 ; i < 6 ; i++ ) {
+            wake_up.add( Calendar.HOUR, 1 );
+            wake_up.add( Calendar.MINUTE, 30 );
+            r += justTheTime( wake_up ) + "\n";
+        }
+        result_text.setText( r );
+    }
+
+    private String justTheTime( Calendar c ) {
+        String r = new String( c.get( Calendar.HOUR_OF_DAY ) + ":" );
+        r += ( ( c.get( Calendar.MINUTE ) < 10 ) ? "0" : "" ) + c.get( Calendar.MINUTE );
+        r = ( (Calendar.getInstance()).get( Calendar.DAY_OF_YEAR ) != c.get( Calendar.DAY_OF_YEAR ) ? "tomorrow" : "today" ) + " at " + r;
+
+        return r;
     }
 }
