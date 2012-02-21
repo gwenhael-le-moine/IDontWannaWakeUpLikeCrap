61c527d884dfa61a6bc67ea1711133d8e56dc871
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2012-02-21 22:21:46 +0100

    useless import

--- src/com/gwenhael/idontwannawakeuplikecrap/IDontWannaWakeUpLikeCrapActivity.java
+++ src/com/gwenhael/idontwannawakeuplikecrap/IDontWannaWakeUpLikeCrapActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View.OnClickListener;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
